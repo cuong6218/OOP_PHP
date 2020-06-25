3c7218d0d8e194974db995a2adba9d3d8f601cd7
cuong6218 <cuong6218@domain.com>
2020-06-25 09:33:52 +0700

    update

--- QuadraticEquation/index.php
+++ QuadraticEquation/index.php
@@ -20,8 +20,7 @@
         $number1 = $_POST['number1'];
         $number2 = $_POST['number2'];
         $number3 = $_POST['number3'];
-        $pt1 = new Quadra($number1, $number2, $number3);
-        
+        $pt1 = new Quadra($number1, $number2, $number3);       
         $pt1->display();
     }
 ?>
